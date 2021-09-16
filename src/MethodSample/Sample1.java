f8287c36bd80177389d518c260ed6ae9062db54b
IT학교 <IT@DESKTOP-D0DOIQA>
2021-09-16 09:25:19 +0900

    gg2

---
+++ src/MethodSample/Sample1.java
@@ -0,0 +1,29 @@
+package MethodSample;
+
+/* method(Java) = function(c) = library 함수 ? 변수 지정 하나의 기능을 모아두는 곳
+ * 
+ * method? 어떤 문제를 해결하는데 필요한 속성(클래스변수), 동작(기능)을 구현할 수 있도록 제공해 주는 것.
+ * 
+ * function = 속성과 기능(행위, 동작)
+ *  - 속성 : 클래스 변수, 자동 초기화, 필드 영역에서 기술
+ *  - 기능 : 클래스 메소드, parameter, arguments, 
+ * 
+ * 
+ */
+
+public class Sample1 {
+//field area : 클래스 변수와 클래스 메소드
+	
+	public static void main(String[] args) {
+		//main 영역 : 지역변수, 지역함수
+		
+		
+		
+		
+	} //main end
+	
+	
+//field area
+	
+	
+}//class end
